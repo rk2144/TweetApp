df1c9b8086d6ecaf9c526324c84918657833a888
RINKU BABU LODHI <rinkulodhi799@gmail.com>
2025-05-13 12:46:46 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+"web: gunicorn chatapi.wsgi" 
--- chatapi/settings.py
+++ chatapi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o1yp12$0t%!rs3hrx)fhjlbjc8#c91(ph7!(7r6i%0*czlk)(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 ]
 
 ROOT_URLCONF = 'chatapi.urls'
@@ -122,8 +123,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticflies')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
